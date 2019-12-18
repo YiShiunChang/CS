73e946d18f5e667c0098e99f18961d5802ac600a
YiShiunChang <43834149+YiShiunChang@users.noreply.github.com>
2019-12-19 01:24:07 +0800

    update Unique

--- FetchRewards/UniqueEmails.java
+++ FetchRewards/UniqueEmails.java
@@ -18,34 +18,34 @@ import java.util.Set;
 public class UniqueEmails {
 	
 	public int getNumUniqueEmails(String[] emails) {
-    Set<String> set = new HashSet<>();
+		Set<String> set = new HashSet<>();
     
-    for (String email: emails) {
-    	if (email.length() == 0) {
-    		continue;
-    	}
-    	
-    	set.add(getUnique(email));
-    }
+		for (String email: emails) {
+			if (email.length() == 0) {
+				continue;
+			}
+			
+			set.add(getUnique(email));
+		}
     
     return set.size();
 	}
 	
 	public String getUnique(String email) {
-	    String[] names = email.split("@");
-	    
-	    StringBuilder localName = new StringBuilder();
-	    for (int i = 0; i < names[0].length(); i++) {
-        char c = names[0].charAt(i);
-        if (c == '+') {
-            break;
-        } else if (c == '.') {
-            continue;
-        }
-        
-        localName.append(c);
-	    }
-	    
-	    return localName.toString() + "@" + names[1];
+		String[] names = email.split("@");
+		StringBuilder localName = new StringBuilder();
+		
+		for (int i = 0; i < names[0].length(); i++) {
+			char c = names[0].charAt(i);
+			if (c == '+') {
+				break;
+			} else if (c == '.') {
+				continue;
+			}
+  
+			localName.append(c);
+		}
+
+		return localName.toString() + "@" + names[1];
 	}
 }
